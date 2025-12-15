fdbabdfbaae721217c0b4713deaff87a9c9af6b1
Jack J Burleson // Lucius Morningstar <exios4@protonmail.com>
2025-12-15 17:52:59 -0600

    Enhance CSS build process and update styles
    - Added new postinstall script for Husky and a fix:css command in package.json to streamline CSS processing.
    - Updated multiple CSS files to ensure consistent styling with Bootstrap v5.3.1, including art-deco.css, cyberpunk.css, dark-solar.css, dark.css, light.css, retrofuturism.css, and tokyo-neon.css.
    - Included autoprefixer and postcss dependencies for improved CSS compatibility and performance.

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {
+      overrideBrowserslist: [
+        'Chrome >= 17',
+        'ChromeAndroid >= 18',
+        'Edge >= 79',
+        'Firefox >= 0',
+        'Safari >= 0',
+        'Opera >= 15',
+        'Samsung Internet >= 6.0'
+      ]
+    }
+  }
+};
+
